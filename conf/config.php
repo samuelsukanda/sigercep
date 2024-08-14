f85b0c2c02c673b7c1984387a9b0ebf23c2c3357
samuelsukanda <samuelsukanda248@gmail.com>
2024-08-14 15:13:54 +0700

    update

--- conf/config.php
+++ conf/config.php
@@ -3,8 +3,9 @@
 $host = "localhost";
 $user = "root";
 $database = "sigercep_db";
+$port = 3366;
 
-$koneksi = new mysqli($host, $user, '', $database);
+$koneksi = new mysqli($host, $user, '', $database, $port);
 
 if (!$koneksi) {
   die("Koneksi gagal: " . mysqli_connect_error());
